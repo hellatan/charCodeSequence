729cfa0a721617c58b2377dfdcf07cb01dfcf774
nameofname <nameofemail@gmail.com>
2018-01-17 15:23:40 -0500

    added more tests for complicated cases

--- test/findMatchFactory_spec.js
+++ test/findMatchFactory_spec.js
@@ -4,30 +4,49 @@ const { expect } = require('chai');
 describe("findMatchFactory", () => {
 
     let findMatch;
-    let called = false;
+    let called = 0;
     const charArr = [1, 2, 3];
     const callback = () => {
-        called = true;
+        ++called;
     };
 
     beforeEach(() => {
-        called = false;
+        called = 0;
         findMatch = findMatchFactory(charArr, callback);
-        console.log('this is a file');
     });
 
     it('should return a function', () => {
         expect(typeof findMatchFactory()).to.equal('function');
     });
 
-    it('should not invoke a callback if the set satisfied', () => {
+    it('should invoke a callback if the set satisfied', () => {
         [1, 2, 3].forEach(findMatch);
-        expect(called).to.equal(true);
+        expect(called).to.equal(1);
     });
 
     it('should not invoke a callback if the set was not satisfied', () => {
         [1, 2, 4].forEach(findMatch);
-        expect(called).to.equal(false);
+        expect(called).to.equal(0);
+    });
+
+    it('should invoke a callback if the a complicated set containing the set is executed', () => {
+        [1, 2, 1, 2, 3].forEach(findMatch);
+        expect(called).to.equal(1);
+    });
+
+    it('should invoke a callback twice if the set was executed twice', () => {
+        [1, 2, 3, 1, 2, 3].forEach(findMatch);
+        expect(called).to.equal(2);
+    });
+
+    it('should invoke a callback twice if the set was executed twice but spaced out', () => {
+        [1, 2, 3, 4, 5, 1, 2, 3].forEach(findMatch);
+        expect(called).to.equal(2);
+    });
+
+    it('should invoke a callback once if the set was executed twice but spaced out', () => {
+        [1, 2, 1, 2, 1, 2, 3, 1].forEach(findMatch);
+        expect(called).to.equal(1);
     });
 
 });
