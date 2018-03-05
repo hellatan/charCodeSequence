d8d8f9b44cde410110c3c299ba0fe2318f8f0f2f
nameofname <nameofemail@gmail.com>
2018-03-05 18:10:48 -0500

    only apply change callbacks if the array changes after keypress

--- index.js
+++ index.js
@@ -6,30 +6,27 @@ const findMatchFactory = require('./src/findMatchFactory');
 
 function charCodeSequence(charArr = [], callback) {
     const findMatch = findMatchFactory(charArr, callback);
-    const changeCallbacks = [];
+    let currLength;
+    findMatch.changeCallbacks = [];
 
     document.addEventListener('keypress', e => { // keypress required to get the correct charCode
         findMatch(e.which);
-        changeCallbacks.forEach(callback => {
-            callback({ event: e, currArr: findMatch.currArr });
-        });
+        if (currLength !== findMatch.currArr.length) {
+            findMatch.changeCallbacks.forEach(callback => {
+                callback({ event: e, currArr: findMatch.currArr });
+            });
+        }
+        currLength = findMatch.currArr.length;
     });
 
-    return {
-        onChange: function onChange (callback) {
-            if (typeof callback === 'function') {
-                changeCallbacks.push(callback);
-            }
+    findMatch.onChange = function onChange (callback) {
+        if (typeof callback === 'function') {
+            findMatch.changeCallbacks.push(callback);
         }
-    };
+    }
+
+    return findMatch;
 }
 
 module.exports = (arr, callback) => charCodeSequence(arr, callback);
 module.exports.konami = (callback) => charCodeSequence(konamiSeries, callback);
-
-
-// const findMatch = charCodeSequence(keyArr, () => { console.log('it was all pressed') });
-
-// findMatch.onChange(({ event, currArr }) => {
-//     console.log(currArr);
-// })
