79d0a943531eaaa40accf8997fb728949b8a383d
nameofname <nameofemail@gmail.com>
2018-03-14 17:54:22 -0400

    es5-ify index file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Key array defaults to the konami code :
-const konamiSeries = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-const findMatchFactory = require('./dist/findMatchFactory');
-const charCodeSequence = require('./dist/charCodeSequence');
+var konamiSeries = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var findMatchFactory = require('./dist/findMatchFactory');
+var charCodeSequence = require('./dist/charCodeSequence');
 
 
-module.exports.listenKeypress = (arr, callback) => charCodeSequence(arr, callback);
-module.exports.konami = (callback) => charCodeSequence(konamiSeries, callback);
+module.exports.listenKeypress = function(arr, callback) { charCodeSequence(arr, callback); };
+module.exports.konami = function(callback) { charCodeSequence(konamiSeries, callback); };
 module.exports.findMatchFactory = findMatchFactory;
